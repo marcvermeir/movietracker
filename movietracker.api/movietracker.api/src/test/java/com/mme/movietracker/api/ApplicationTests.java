ddf2b35b72f1e8618bf830823d734e03d0bed267
Marc Vermeir <marcvermeir@pandora.be>
2021-01-12 20:27:40 +0100

    20210112_2030

---
+++ movietracker.api/movietracker.api/src/test/java/com/mme/movietracker/api/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mme.movietracker.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
