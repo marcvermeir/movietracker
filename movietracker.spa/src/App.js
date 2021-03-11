d05898bee4c7b023a081e4fc20e7b895da9b4b8d
Marc Vermeir <marcvermeir@pandora.be>
2021-03-11 21:17:00 +0100

    20210311_2116

--- movietracker.spa/src/App.js
+++ movietracker.spa/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+// import default style
+// import 'rsuite/lib/styles/index.less'; // or 'rsuite/dist/styles/rsuite-default.css'
+import 'rsuite/dist/styles/rsuite-default.css';
+
+import { Button } from 'rsuite';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button>Hello World</Button>
     </div>
   );
 }
