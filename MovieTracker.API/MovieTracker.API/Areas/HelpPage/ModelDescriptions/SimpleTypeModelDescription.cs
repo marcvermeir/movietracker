8c4f06cf08573b7f84f2784fb354ac4739b9b298
marc vermeir <marcvermeir@pandora.be>
2023-03-17 21:07:05 +0100

    20230317_2106

---
+++ MovieTracker.API/MovieTracker.API/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace MovieTracker.API.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ MovieTracker.API/MovieTracker.API/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MovieTracker.API.WebApiApplication" Language="C#" %>
