5e10d909674522652a7578f0fb57f92981fd8051
marc vermeir <marcvermeir@pandora.be>
2023-11-16 20:43:50 +0100

    20231116_2043

---
+++ AspNETCoreWebApi7/AspNETCoreWebApi7/Models/Movie.cs
@@ -0,0 +1,15 @@
+﻿namespace AspNETCoreWebApi7.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+
+        public string? Title { get; set; }
+
+        public string? Description { get; set; }
+        
+        public string? Genre { get; set; }
+        
+        public DateTime ReleaseDate { get; set; }   
+    }
+}
