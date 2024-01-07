7c49d38febe8c5859764f6760f5cc5152a402db9
marc vermeir <marcvermeir@pandora.be>
2024-01-07 16:25:53 +0100

    20240107_1625

---
+++ MediaTracker.2023/Core/Entities/User.cs
@@ -0,0 +1,22 @@
+﻿namespace Core.Entities
+{
+    /// <summary>
+    /// Represents a User.
+    /// </summary>
+    public class User : IEntity, IAggregateRoot
+    {
+        #region Properties
+
+        /// <summary>
+        /// The unique ID of the User.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// The Username of the User.
+        /// </summary>
+        public required string Username { get; set; }
+
+        #endregion
+    }
+}
