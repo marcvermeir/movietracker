9bbd7cd7c7a4ad6597c8be8ded6036cd213b4953
marc vermeir <marcvermeir@pandora.be>
2024-05-03 22:14:24 +0200

    20240503_2214

--- TodoTracker/CourseLibrary.API/Models/TodoItemForCreationDto.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace TT.Api.Models;
-
-public class TodoItemForCreationDto
-{
-    public string Title { get; set; } = string.Empty;
-    public string Description { get; set; } = string.Empty;
-}
--- TodoTracker/CourseLibrary.API/Models/TodoListDto.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace TT.Api.Models;
-
-public class TodoListDto
-{
-    public Guid Id { get; set; }
-    public string Title{ get; set; } = string.Empty;
-}
