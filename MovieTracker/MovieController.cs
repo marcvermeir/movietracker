91c8773b4c80136ad0275d470db2b45262c56bcc
marc vermeir <marcvermeir@pandora.be>
2024-11-13 21:27:26 +0100

    20241113_2127

---
+++ MovieTracker/MovieController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieTracker.Models;
+
+namespace MovieTracker
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MovieController : ControllerBase
+    {
+        private readonly MovieCtx _ctx;
+
+        public MovieController(MovieCtx ctx)
+        {
+            _ctx = ctx ?? throw new ArgumentNullException(nameof(ctx));
+        }
+
+        // Read
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Movie>> GetMovie(int id)
+        {
+            var movie = await _ctx.Movies.FindAsync(id);
+
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            return movie;
+        }
+    }
+}
---
+++ MovieTracker/MovieCtx.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieTracker.Models;
+
+namespace MovieTracker
+{
+    public class MovieCtx : DbContext
+    {
+        public MovieCtx(DbContextOptions<MovieCtx> options) : base(options) { }
+
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
