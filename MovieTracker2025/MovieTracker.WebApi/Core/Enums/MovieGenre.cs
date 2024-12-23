2fc6ff40b689ade50066b20275cd61b32f8b607f
marc vermeir <marcvermeir@pandora.be>
2024-12-23 21:21:17 +0100

    20241223_2121

---
+++ MovieTracker2025/MovieTracker.WebApi/Core/Enums/MovieGenre.cs
@@ -0,0 +1,16 @@
+﻿
+namespace MovieTracker.WebApi.Core.Enums
+{
+    public enum MovieGenre
+    {
+        Action,
+        Comedy,
+        Drama,
+        Fantasy,
+        Horror,
+        Mystery,
+        Romance,
+        Thriller,
+        Western
+    }
+}
