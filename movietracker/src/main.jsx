09428672b4293994e22d1a10dbbe74f093a7cea1
marcvermeir <marcvermeir@pandora.be>
2025-07-28 21:20:40 +0200

    20250728_2120

--- movietracker/src/main.jsx
+++ movietracker/src/main.jsx
@@ -2,14 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from "react-router-dom";
-import { ChakraProvider } from '@chakra-ui/react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-    
-        <App />
-    </BrowserRouter>
-  </StrictMode>
+    <App />
+  </StrictMode>,
 )
